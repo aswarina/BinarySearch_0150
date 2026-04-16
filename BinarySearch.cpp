88a7c2ecc01a30961e6ef82b24132d3981bf69ca
aswarina <lyxsyyawa@gmail.com>
2026-04-16 10:38:18 +0700

    tamah fungsi input dan validasi panjang array

--- BinarySearch.cpp
+++ BinarySearch.cpp
@@ -4,3 +4,22 @@ using namespace std;
 int element[10];
 int nPanjang;
 int x;
+
+void input()
+{
+    while(true)
+    {
+        cout << "Masukkan banyaknya elemen pada array (maksiml 10): ";
+        cin >> nPanjang;
+
+        if (nPanjang <= 10)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\n[!] Jumlah elemen tidak leh lebih dari 10. Silakan coba lagi.\n";
+        
+        }
+    }
+}
